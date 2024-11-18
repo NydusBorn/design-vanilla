14cd0e98eef86900057cacb220f93894ccbc92d6
NydusBorn <nydusborn@outlook.com>
2024-11-18 22:51:28 +0300

    add navbar movement, create splash advert

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import './style.scss'
+
+window.addEventListener("scroll",() => {
+    const navbar = document.querySelector("nav")!
+    if (window.scrollY >= 450){
+        navbar.style.backdropFilter = "blur(10px)"
+        navbar.style.marginTop = `${window.scrollY}px`
+    }
+    else{
+        navbar.style.backdropFilter = ""
+        navbar.style.marginTop = ""
+    }
+})
